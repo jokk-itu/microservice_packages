b7acfd805303718f57de933c35839bae72eb1f22
Joachim Køcher Kelsen <joachim@kelsen.nu>
2021-09-09 22:20:09 +0200

    add: unhealthy information

--- Jokk.Microservice.Prometheus/HealthChecks/ServiceHealthCheck.cs
+++ Jokk.Microservice.Prometheus/HealthChecks/ServiceHealthCheck.cs
@@ -12,20 +12,23 @@ namespace Jokk.Microservice.Prometheus.HealthChecks
         private readonly string _service;
         private readonly string _uri;
         private readonly HttpClient _httpClient;
-        
+
         public ServiceHealthCheck(IHttpClientFactory factory, string service, string uri)
         {
             _service = service;
             _uri = uri;
             _httpClient = factory.CreateClient(ClientName.HealthCheck);
+            _httpClient.BaseAddress = new Uri(_uri);
         }
-        
-        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+            CancellationToken cancellationToken = new CancellationToken())
         {
-            var response = await _httpClient.GetAsync(new Uri($"{_uri}/{HealthCheckEndpoint.Endpoint}"), cancellationToken);
-            return response.IsSuccessStatusCode 
-                ? HealthCheckResult.Healthy($"Service {_service} is healthy") 
-                : HealthCheckResult.Unhealthy($"Service {_service} is unhealthy");
+            var response = await _httpClient.GetAsync(HealthCheckEndpoint.Endpoint, cancellationToken);
+            return response.IsSuccessStatusCode
+                ? HealthCheckResult.Healthy($"Service {_service} is healthy")
+                : HealthCheckResult.Unhealthy(
+                    $"Service {_service} is unhealthy, Status {response.StatusCode}, Reason {response.ReasonPhrase}");
         }
     }
 }
